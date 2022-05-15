1ed73a79ccea86c041de97964b948efbfb8bf4e7
phinic925 <phinicnyangate480@gmail.com>
2022-05-15 10:36:49 +0300

    add the result page

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ function result(){
     var q3 = document.script.question3.value;
     var q4 = document.script.question4.value;
     var q5 = document.script.question5.value;
-   
+    var result= document .getElementById('result');
+    var script = document .getElementById("quizz");
     
     if (q1=="Variables") {b++}
     if (q2=="object-oriented") {b++}
     if (q3=="const") {b++}
     if (q4=="let") {b++}
     if (q5== "in") {b++}
-    document .write(b);
+    
+    script.style .display ="none";
+    if (b<=3) {
+        result.textContent ='Your score is ${b}. Try to improve next time.'
+    }
+    else {
+        result .textContent= 'Your score is ${b}. Congrats!'
+    }
+    
 }
